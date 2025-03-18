c6450d7aaf14f5de5ed8fcbd7467209d27d44986
a1916700 <zihan.luo@student.adelaide.edu.au>
2025-03-18 18:15:22 +1030

    front-end init

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+        Recommendation System
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
