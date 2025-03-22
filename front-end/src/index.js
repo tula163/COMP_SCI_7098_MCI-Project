dbf56a0d777fcd2226c3356fbd9753b6fa8bc2a3
a1916700 <zihan.luo@student.adelaide.edu.au>
2025-03-22 21:12:08 +1030

    project

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+// import App from './App';
+// import { Provider } from 'react-redux';
+import {
+  RouterProvider,
+} from "react-router";
+import router from './routes';
+// import store from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+  {/* <Provider store={store}> */}
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  {/* </Provider> */}
   </React.StrictMode>
 );
 
---
+++ front-end/src/pages/layout/index.js
@@ -0,0 +1,7 @@
+
+
+const Layout = ()=>{
+    return <div className="layout"> Recommendation System</div>
+}
+
+export default Layout
