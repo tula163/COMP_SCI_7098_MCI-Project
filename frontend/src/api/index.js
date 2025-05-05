7a8c71ad43fc42d041063c72824eeec13ccf9ea2
a1916700 <zihan.luo@student.adelaide.edu.au>
2025-05-05 11:39:30 +0930

    week 7 minutes

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080', // 后端Spring Boot服务地址
-  timeout: 5000, // 请求超时时间
+  baseURL: 'http://localhost:8080', 
+  timeout: 5000, 
   headers: {
     'Content-Type': 'application/json'
   }
