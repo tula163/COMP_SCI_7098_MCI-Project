0ba1265122d87296d8d2eba54bc4e7f0e8f97509
tula163 <simon_tula@163.com>
2025-06-06 01:42:20 +0930

    delete old version

--- Code/Back-end/src/main/java/com/example/backcode/repository/RequireRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backcode.repository;
-
-import com.example.backcode.pojo.Requirements;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequireRepository extends JpaRepository<Requirements, Long> {
-    // 无需额外代码，继承 JpaRepository 即可
-}
