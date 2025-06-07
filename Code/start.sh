ae6db103878ac7e43ba2296ab0b538306bcefaa4
a1916700 <zihan.luo@student.adelaide.edu.au>
2025-06-08 00:35:44 +0930

    change readme

--- Code/start.sh
+++ Code/start.sh
@@ -3,19 +3,8 @@
 
 # 1. start backend
 cd back || exit
-
-# Create a virtual environment（if not）
-if [ ! -d ".venv" ]; then
-  echo "Creat Python virtual environment .venv..."
-  python3 -m venv .venv
-fi
-
-# Install the back-end dependencies
-pip install -r requirements.txt
-
 source .venv/bin/activate
 
-pip install -r requirements.txt
 
 # start Django
 python manage.py runserver 0.0.0.0:8000 &
@@ -24,7 +13,6 @@ BACK_PID=$!
 # 2. start frontend
 cd ../Front-end || { echo "Please check your path"; exit 1; }
 
-npm install
 npm start &
 FRONT_PID=$!
 
